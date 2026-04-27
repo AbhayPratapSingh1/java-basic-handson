1bf65f1277d75ca51a76253eed27485471c2ae02
AbhayPratapSingh1 <abhaypratapsingh481833@gmail.com>
2026-04-27 15:07:24 +0530

    implements money and string class

--- Money.java
+++ Money.java
@@ -1,1 +1,13 @@
 // Money.java
+public class Money {
+  final int value;
+  Money(int amount){
+    this.value = amount;
+  }
+  public Money add(Money money){
+    return new Money(this.value + money.value);
+  }
+  public Money minus(Money money){
+    return new Money(this.value - money.value);
+  }
+}
--- StringUtils.java
+++ StringUtils.java
@@ -1,1 +1,30 @@
 // StringUtils
+public class StringUtils{
+  public static boolean containsChar(String word, char c){
+    
+    for (int i = 0; i < word.length(); i++) {
+      if (word.charAt(i) == c){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static String reverse(String word){
+    String reversed = "";
+    for (int i = word.length() - 1; i >= 0; i--) {
+      reversed = reversed +  word.charAt(i);
+    }
+    return reversed;
+  }
+
+  public static String replaceAll(String word, char from, char to){
+    String replaced = "";
+    for (int i = 0 ; i < word.length(); i++) {
+      char c = word.charAt(i);
+      replaced += c == from ? to : c;
+    }
+
+    return replaced;
+  }
+}
